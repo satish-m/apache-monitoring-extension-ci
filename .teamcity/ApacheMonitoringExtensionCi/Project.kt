243e07f7aff1050542c64604868fb2ac8f848eec
satish.muddam <satish.muddam@appdynamics.com>
2018-07-11 15:41:19 +0530

    Adding publish buildType

--- .teamcity/ApacheMonitoringExtensionCi/Project.kt
+++ .teamcity/ApacheMonitoringExtensionCi/Project.kt
@@ -17,6 +17,8 @@ object Project : Project({
     buildType(ApacheMonitoringExtensionCi_Clean)
     buildType(ApacheMonitoringExtensionCi_Compile_Test_Java7)
     buildType(ApacheMonitoringExtensionCi_Compile_Test_Java8)
+    buildType(ApacheMonitoringExtensionCi_Publish)
+
 
     features {
         versionedSettings {
