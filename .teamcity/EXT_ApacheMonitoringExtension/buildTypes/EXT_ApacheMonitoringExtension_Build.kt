41e5626075e7ee6323dc48cefcce64d75a24b86b
satish.muddam <satish.muddam@appdynamics.com>
2018-11-13 16:04:25 +0530

    Merge branch 'master' of https://github.com/satish-m/apache-monitoring-extension-ci
    # Conflicts:
    #	.teamcity/EXT_ApacheMonitoringExtension/Project.kt

--- .teamcity/EXT_ApacheMonitoringExtension/buildTypes/EXT_ApacheMonitoringExtension_Build.kt
+++ .teamcity/EXT_ApacheMonitoringExtension/buildTypes/EXT_ApacheMonitoringExtension_Build.kt
@@ -3,13 +3,16 @@ package EXT_ApacheMonitoringExtension.buildTypes
 import jetbrains.buildServer.configs.kotlin.v2017_2.*
 import jetbrains.buildServer.configs.kotlin.v2017_2.triggers.vcs
 
+/**
+ * @author Satish Muddam
+ */
 object EXT_ApacheMonitoringExtension_Build : BuildType({
     uuid = "8ae04611-4cb4-4e71-97ac-95570f17f4b1"
     id = "EXT_ApacheMonitoringExtension_Build"
     name = "Build"
 
     vcs {
-        root(EXT_ApacheMonitoringExtension.vcsRoots.EXT_ApacheMonitoringExtension_HttpsGithubComSatishMApacheMonitoringExtensionCiRe)
+        root(EXT_ApacheMonitoringExtension.vcsRoots.EXT_ApacheMonitoringExtension_Apache)
 
     }
 
