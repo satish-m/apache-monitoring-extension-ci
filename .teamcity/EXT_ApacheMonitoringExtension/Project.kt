5120dc415cf4f0b50166c1c39f992ebfe4cf7f0f
admin <admin@TeamCity>
2018-11-13 14:46:47 +0530

    TeamCity change in 'Extensions :: Apache Monitoring Extension' project: Versioned settings configuration updated

--- .teamcity/EXT_ApacheMonitoringExtension/Project.kt
+++ .teamcity/EXT_ApacheMonitoringExtension/Project.kt
@@ -1,26 +1,22 @@
 package EXT_ApacheMonitoringExtension
 
-import EXT_ApacheMonitoringExtension.vcsRoots.*
-import EXT_ApacheMonitoringExtension.vcsRoots.EXT_ApacheMonitoringExtension_ApacheMonitoringExtension
 import jetbrains.buildServer.configs.kotlin.v2017_2.*
 import jetbrains.buildServer.configs.kotlin.v2017_2.Project
 import jetbrains.buildServer.configs.kotlin.v2017_2.projectFeatures.VersionedSettings
 import jetbrains.buildServer.configs.kotlin.v2017_2.projectFeatures.versionedSettings
 
 object Project : Project({
-    uuid = "95e2aa25-051d-4109-88a4-297b9949ac91"
+    uuid = "8342fb3f-7885-4eb9-8d23-efad7f3b0b06"
     id = "EXT_ApacheMonitoringExtension"
     parentId = "EXT"
     name = "Apache Monitoring Extension"
 
-    vcsRoot(EXT_ApacheMonitoringExtension_ApacheMonitoringExtension)
-
     features {
         versionedSettings {
-            id = "PROJECT_EXT_6"
+            id = "PROJECT_EXT_7"
             mode = VersionedSettings.Mode.ENABLED
             buildSettingsMode = VersionedSettings.BuildSettingsMode.PREFER_SETTINGS_FROM_VCS
-            rootExtId = EXT_ApacheMonitoringExtension_ApacheMonitoringExtension.id
+            rootExtId = "EXT_ApacheMonitoringExtension"
             showChanges = false
             settingsFormat = VersionedSettings.Format.KOTLIN
             storeSecureParamsOutsideOfVcs = true
