a51d233e0b42e51b6183b97fbe9c358cab642517
satish.muddam <satish.muddam@appdynamics.com>
2018-11-21 19:12:10 +0530

    Creating new buildSteps

--- .teamcity/EXT_ApacheMonitoringExtension/buildTypes/EXT_ApacheMonitoringExtension_VerifyPipeline.kt
+++ .teamcity/EXT_ApacheMonitoringExtension/buildTypes/EXT_ApacheMonitoringExtension_VerifyPipeline.kt
@@ -8,7 +8,6 @@ import jetbrains.buildServer.configs.kotlin.v2017_2.triggers.vcs
  * @author Satish Muddam
  */
 object EXT_ApacheMonitoringExtension_VerifyPipeline : BuildType({
-    template = "VerifyPipeline"
     uuid = "35C83B30-3647-4A03-B729-1B531B9C6C0C"
     id = "EXT_ApacheMonitoringExtension_VerifyPipeline"
     name = "Verify Pipeline"
